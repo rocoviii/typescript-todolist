e72ece7e7cf4acc4c0db9372877c9a7c3c2f14d9
rocoviii <2merry3u@naver.com>
2022-08-10 23:51:07 +0900

    todo-c

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,16 +2,32 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import TodoListStyled from "./TodoList.styled";
 import { useTodosState } from "../contexts/TodosContext";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function TodoList() {
   const todos = useTodosState();
+  const onDragEnd = (e) => {
+    if (!e.destination) {
+      return;
+    }
+  };
   return (
     <TodoListStyled className="todoList">
-      {todos.length === 0 ? (
-        <span>오늘 할 일을 등록하세요!</span>
-      ) : (
-        todos.map((todo) => <TodoItem todo={todo} key={todo.id} />)
-      )}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {todos.length === 0 ? (
+                <span>오늘 할 일을 등록하세요!</span>
+              ) : (
+                todos.map((todo, index) => (
+                  <TodoItem todo={todo} key={todo.id} />
+                ))
+              )}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </TodoListStyled>
   );
 }
